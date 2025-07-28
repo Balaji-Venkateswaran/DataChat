60a68c2cedf8aa5e9e8c11b8213a8e11cbb7ac63
Dinesh <dinesh.nallaiya@cannyfore.com>
2025-07-28 18:26:51 +0530

    Implemented table structure concept

--- dataclientapp/app/js/index.js
+++ dataclientapp/app/js/index.js
@@ -1,21 +1,24 @@
 
+const tableStructure = []
 
 function uploadFile(event) {
-  const file = event.target.files[0];
-  
-  const fileNameArr = file.name;
-  const fileNameExtacter = fileNameArr.split('.')
-  const fileName = fileNameExtacter[fileNameExtacter.length-1];
-  console.log("Uploaded File:", file,fileName);
-  switch(fileName){
-    case 'db':
-      extractDBfile(file);
-    break;
-    case 'xlsx':
-    case 'xls' :
-      extractXlsxFile(file);
-    break;
+  for (let i = 0; i < event.target.files.length; i++) {
+    const file = event.target.files[i];
+    const fileNameArr = file.name;
+    const fileNameExtacter = fileNameArr.split('.')
+    const fileName = fileNameExtacter[fileNameExtacter.length - 1];
+    console.log("Uploaded File:", file, fileName);
+    switch (fileName) {
+      case 'db':
+        extractDBfile(file);
+        break;
+      case 'xlsx':
+      case 'csv':
+        extractXlsxFile(file);
+        break;
+    }
   }
+
 
 }
 
@@ -27,42 +30,65 @@ function extractDBfile(file) {
     reader.onload = function () {
       const uInt8Array = new Uint8Array(reader.result);
       const db = new SQL.Database(uInt8Array);
-     
       const result = db.exec("SELECT name FROM sqlite_master WHERE type='table';");
-      console.log(result)
+      console.log("result", result)
       if (!result.length) {
         console.log("No tables found.");
         return;
       }
-      const tableNames = result[0].values.flat();
 
-      tableNames.forEach(table => {
-        const pragma = db.exec(`PRAGMA table_info(${table});`);
-        console.log(`Table: ${table}`);
-        pragma[0].values.forEach(row => {
-          const [cid, name, type, notnull, dflt_value, pk] = row;
-          console.log(`  Column: ${name}, Type: ${type}, Not Null: ${notnull}, Default: ${dflt_value}, PK: ${pk}`);
+      result.forEach((teble, i) => {
+        const tableNames = result[i].values.flat();
+        tableNames.forEach(table => {
+          const pragma = db.exec(`PRAGMA table_info(${table});`);
+          console.log(`Table: ${table}`);
+          let tempArr = {
+            tableNames: table,
+            tableInfo: []
+          }
+          pragma[0].values.forEach(row => {
+            const [cid, name, type, notnull, dflt_value, pk] = row;
+            tempArr.tableInfo.push({
+              Column: name,
+              Type: type,
+              Default: dflt_value,
+              PK: pk,
+              Not_Null: notnull
+            })
+            console.log(`  Column: ${name}, Type: ${type}, Not Null: ${notnull}, Default: ${dflt_value}, PK: ${pk}`);
+          });
+          tableStructure.push(tempArr)
         });
-      });
+      })
+      console.log(tableStructure)
     };
-
     reader.readAsArrayBuffer(file);
+
   });
+
 }
 
-function extractXlsxFile(e){
-// document.getElementById("excelFile").addEventListener("change", function (e) {
-    const reader = new FileReader();
-    reader.onload = function (e) {
-      const data = new Uint8Array(e.target.result);
-      const workbook = XLSX.read(data, { type: "array" });
-      console.log(workbook)
-      const sheetName = workbook.SheetNames[1];
+function extractXlsxFile(e) {
+  const reader = new FileReader();
+  reader.onload = function (e) {
+    const data = new Uint8Array(e.target.result);
+    const workbook = XLSX.read(data, { type: "array" });
+    console.log("workbook", workbook)
+
+    for (let i = 0; i < workbook?.SheetNames.length; i++) {
+      tempArr = {
+        tableName: '',
+        tableInfo: []
+      }
+      const sheetName = workbook.SheetNames[i];
+
+      console.log("sheet", sheetName)
       const worksheet = workbook.Sheets[sheetName];
+      tempArr.tableName = sheetName
       const jsonData = XLSX.utils.sheet_to_json(worksheet, { header: 1 });
 
-      const headers = jsonData[0]; // First row = column names
-      const firstRow = jsonData[1]; // Sample row
+      const headers = jsonData[0];
+      const firstRow = jsonData[1];
       const inferredTypes = headers.map((header, i) => {
         const val = firstRow[i];
         if (val == null) return "NULL";
@@ -71,11 +97,33 @@ function extractXlsxFile(e){
 
       const schema = headers.map((col, i) => ({
         column: col,
-        type: inferredTypes[i]
+        type: inferredTypes[i],
+        Default: "NA",
+        PK: "NA",
+        Not_Null: "NA"
       }));
-
-      console.log("Inferred Table Schema:", schema);
-    };
-    reader.readAsArrayBuffer(e);
-  // });
+      tempArr.tableInfo.push(schema)
+      tableStructure.push(tempArr)
+    }
+    console.log("Inferred Table Schema:", tableStructure);
+  };
+  reader.readAsArrayBuffer(e);
 }
+
+
+
+// unused code
+// const file = event.target.files[0];
+// const fileNameArr = file.name;
+// const fileNameExtacter = fileNameArr.split('.')
+// const fileName = fileNameExtacter[fileNameExtacter.length-1];
+// console.log("Uploaded File:", file,fileName);
+// switch(fileName){
+//   case 'db':
+//     extractDBfile(file);
+//   break;
+//   case 'xlsx':
+//   case 'xls' :
+//     extractXlsxFile(file);
+//   break;
+// }
