0a74c6bef30ce60cdb4ac26c091e40fff0fd4c61
sowrimalaiyan.m <sowrimalaiyan.m@cannyfore.com>
2025-07-29 07:12:23 +0530

    upload-file

--- dataserverapp/app/services/file_service.py
+++ dataserverapp/app/services/file_service.py
@@ -5,11 +5,25 @@ from fastapi import UploadFile
 UPLOAD_DIR = "uploads"
 os.makedirs(UPLOAD_DIR, exist_ok=True)
 
-async def save_upload_file(file: UploadFile) -> str:
-    file_path = os.path.join(UPLOAD_DIR, str(file.filename))
+import os
+import shutil
+from fastapi import UploadFile, HTTPException
 
+UPLOAD_DIR = "uploads"
+os.makedirs(UPLOAD_DIR, exist_ok=True)
+
+async def save_upload_file(file: UploadFile) -> str:
+
+    if not file.filename:
+        raise HTTPException(status_code=400, detail="No file uploaded.")
+    content = await file.read()
+    if len(content) == 0:
+        raise HTTPException(status_code=400, detail="Uploaded file is empty.")
+    file.file.seek(0)
+    file_path = os.path.join(UPLOAD_DIR, str(file.filename))
     with open(file_path, "wb") as buffer:
         shutil.copyfileobj(file.file, buffer)
-
     return file_path
+
+
 
