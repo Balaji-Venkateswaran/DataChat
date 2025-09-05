70c0e099fcb68a90b7b026fb6754605b6f50aa77
Balaji <balaji.v@cannyfore.com>
2025-09-05 18:17:33 +0530

    upload

--- enhancement/src/components/TextArea.tsx
+++ enhancement/src/components/TextArea.tsx
@@ -1,12 +1,12 @@
-import React from 'react';
-import { InputBase } from '@mui/material';
+import React from "react";
+import { InputBase } from "@mui/material";
 
 export default function TextArea() {
   return (
     <InputBase
-      sx={{ ml: 2, flex: 1}}
+      sx={{ ml: 2, flex: 1 }}
       placeholder="Enter your question here..."
-      inputProps={{ 'aria-label': 'ask anything' }}
+      inputProps={{ "aria-label": "ask anything" }}
     />
   );
 }
